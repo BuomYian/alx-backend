6a03b5b469542614fe644d63724c1589b7f680ab
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-27 11:12:43 +0300

    fixed

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -15,10 +15,10 @@ class LFUCache(BaseCaching):
         super().__init__()
         self.frequencies = {}  # To store the frequency of each key
         self.min_frequency = 0
-        self.order = []  # To store the order of key access
 
     def put(self, key, item):
-        """ Add an item in the cache using LFU algorithm
+        """
+        Add an item in the cache using LFU algorithm
         """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
@@ -27,20 +27,18 @@ class LFUCache(BaseCaching):
                     k for k in self.frequencies if self.frequencies[k] == self.min_frequency]
                 if not lfu_keys:
                     # If no keys with the least frequency, use LRU to break the tie
-                    lru_key = min(
-                        self.order, key=lambda k: self.order.index(k))
+                    lru_key = min(self.frequencies,
+                                  key=lambda k: self.frequencies[k])
                     lfu_keys = [
                         k for k in self.frequencies if self.frequencies[k] == self.frequencies[lru_key]]
 
                 lfu_key = lfu_keys[0]
                 self.cache_data.pop(lfu_key)
                 self.frequencies.pop(lfu_key)
-                self.order.remove(lfu_key)
-                print("DISCARD:", lfu_key)
 
             self.cache_data[key] = item
             self.frequencies[key] = 1
-            self.order.append(key)
+            self.min_frequency = 1
 
     def get(self, key):
         """ Get an item by key
@@ -52,7 +50,7 @@ class LFUCache(BaseCaching):
         self.frequencies[key] += 1
 
         # Move the accessed key to the end
-        self.order.remove(key)
-        self.order.append(key)
+        # self.order.remove(key)
+        # self.order.append(key)
 
         return self.cache_data[key]
