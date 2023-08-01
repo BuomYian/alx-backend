42b16e8eeffe35911cc8843407fe32e8343043f1
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-01 20:28:59 +0300

    changes

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -12,6 +12,9 @@ babel = Babel(app)
 
 # Config class with available language
 class Config:
+    """
+    Represents a Flask Babel configuration.
+    """
     LANGUAGE = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
