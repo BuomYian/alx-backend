3eb7823314259375d7ecac386ea03489c221072d
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-01 19:48:32 +0300

    Flask app with Flask-babel configuration

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Flask-babel configuration
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+# Config class with available language
+class Config:
+    LANGUAGE = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """
+    Renders the index.html template.
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
