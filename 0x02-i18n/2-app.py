08cf95a8d4190e05de531314003dda40e0ba8151
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-01 21:16:28 +0300

    remove unused variable

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,7 +4,7 @@ Basic Flask app with Flask-babel configuration
 """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, babel_localeselector
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
